9068994747231e241fe5cfec45be73a56369a54f
Nurdiansyah15 <nurdinurdiansyah15@gmail.com>
2025-06-13 10:19:37 +0700

    hehe

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./",
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://apihertodanalcom-4af8450eb240.nevacloud.io',
-    }
-  }
-})
+});
